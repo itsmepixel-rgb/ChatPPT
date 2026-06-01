3f57ec3b9d2539a4fe30538c8d968dc67582dd96
Pixel <itsgyphai@gmail.com>
2026-06-01 14:08:50 +0530

    Implement custom API request handler
    Add custom request handler and configuration.

--- api/[[...path]].ts
+++ api/[[...path]].ts
@@ -1,3 +1,13 @@
 import app from "../server";
 
-export default app;
+export const config = {
+  maxDuration: 30,
+};
+
+export default function handler(req: any, res: any) {
+  if (typeof req.url === "string" && !req.url.startsWith("/api/")) {
+    req.url = `/api${req.url.startsWith("/") ? "" : "/"}${req.url}`;
+  }
+
+  return app(req, res);
+}
